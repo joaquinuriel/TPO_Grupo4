06f4d75fd44c17b6fd811a44ac6f108ec5056edf
RoxanaRodriguez <rrodriguezbetbeze@gmail.com>
2024-11-03 16:17:05 -0300

    ultimos cambios 16.15

--- frontend/src/components/WelcomeScreen.js
+++ frontend/src/components/WelcomeScreen.js
@@ -1,38 +1,36 @@
 // WelcomeScreen.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import SplashScreen from './SplashScreen';
 import './WelcomeScreen.css';
+import ModoFitnessON from './ModoFitnessON.png';
 
 function WelcomeScreen() {
-    const [fadeOut, setFadeOut] = useState(false); // Declaramos el estado fadeOut
-    const [showSplash, setShowSplash] = useState(false);
+    const [fadeOut, setFadeOut] = useState(false);
     const navigate = useNavigate();
 
     const handleToggleClick = () => {
-        setFadeOut(true); // Activa el efecto de desvanecimiento
+        setFadeOut(true);
         setTimeout(() => {
             navigate('/home');
-        }, 1500); // Asegúrate de que coincida con el tiempo de la animación de desvanecimiento
+        }, 1500);
     };
 
     return (
-        showSplash ? (
-            <SplashScreen />
-        ) : (
-            <div className={`welcome-screen ${fadeOut ? 'fade-out' : ''}`}>
-                <h1 className="welcome-logo">GYMPAL</h1>
-                <p className="welcome-mode">MODO FITNESS ON</p>
-                <div className="toggle-switch">
-                    <input
-                        type="checkbox"
-                        id="fitness-toggle"
-                        onClick={handleToggleClick}
-                    />
-                    <label htmlFor="fitness-toggle"></label>
-                </div>
+        <div className={`welcome-screen ${fadeOut ? 'fade-out' : ''}`}>
+            <img src={ModoFitnessON} alt="Modo Fitness On" className="fitness-image" />
+            <div className="toggle-switch">
+                <input
+                    type="checkbox"
+                    id="fitness-toggle"
+                    onClick={handleToggleClick}
+                />
+                <label htmlFor="fitness-toggle"></label>
             </div>
-        )
+            <div className="pattern-corner top-left"></div>
+            <div className="pattern-corner top-right"></div>
+            <div className="pattern-corner bottom-left"></div>
+            <div className="pattern-corner bottom-right"></div>
+        </div>
     );
 }
 
